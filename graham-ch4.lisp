90260f83d180f4001b18c5df769a18c2cb7edf5b
Bill Huynh <billhuynh@TQBH>
2023-08-21 11:17:40 -0600

    graham-ch4.lisp, developing

--- graham-ch4.lisp
+++ graham-ch4.lisp
@@ -5,6 +5,13 @@
 
 
 ;; 4.1 - Arrays
+(defparameter arr (make-array '(2 3) :initial-element nil))
+					; should initialize using
+					; initial-element
+arr
 
+(aref arr 0 0)				; retrieve an array element
+(setf (aref arr 0 0) 'b)		; replace, cannot use setq
+arr
 
-
+#2a((b nil nil) (nil nil nil))		; ILLEGAL FUNCTION CALL
